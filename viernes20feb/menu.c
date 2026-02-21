a1bd072e999c9a7ccf51463102405977d6565149
Nestor Solorzano Medina <nestor.solorzano6038@alumnos.udg.mx>
2026-02-20 21:20:52 -0600

    Create menu.c

---
+++ viernes20feb/menu.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int opcion;
+
+    do{
+        printf("\n1. Continuar\n");
+        printf("2. salir\n");
+        scanf("%d", &opcion);
+        }
+            while(opcion != 2);
+
+return 0;
+    }
