38ca6b6fe0e8fccccb6c786c7ea1609fed786869
Nestor Solorzano Medina <nestor.solorzano6038@alumnos.udg.mx>
2026-03-06 07:55:28 -0600

    Add files via upload

---
+++ viernes06marz/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float temp1, temp2;
+    float temp[24];
+    int dummy;
+
+    printf("Dirrecion de mem de 2 flotantes:\n%i\n%i\n", &temp1, &temp2);
+    printf("Direecion de temp[24]: %i\n", temp);
+    printf("Direecion de temp[24]: %i\n", &temp[24]);
+    printf("Dirrecion de dummy: %i\n", &dummy);
+
+    return 0;
+}
